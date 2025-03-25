c2a4e5b684369c7f3096d30aa28a9cd19f90ad71
KhoaLe <levodangkhoatg2@gmail.com>
2025-03-25 16:34:23 +0700

    fix locale

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     i18n: {
         locales: ['vi', 'en'],
         defaultLocale: 'vi',
-        localDetection: false
+        localeDetection: false
     },
     eslint: {
         ignoreDuringBuilds: true,
